73aae45b8ce938a6c4272983cda8b4966a92ffc7
Flomi2010 <florianbruse@outlook.de>
2026-06-28 01:27:01 +0200

    Update OceanRuin.java loottable to 26.1.2 and made Fishing_Rod enchanted loot

--- src/main/java/dev/skippaddin/allAndOnlyChests/structures/OceanRuin.java
+++ src/main/java/dev/skippaddin/allAndOnlyChests/structures/OceanRuin.java
@@ -11,17 +11,28 @@ public class OceanRuin extends Structure {
         put(Material.COAL, false);
         put(Material.WHEAT, false);
         put(Material.GOLD_NUGGET, false);
+        put(Material.ROTTEN_FLESH, false);
         put(Material.FILLED_MAP, false);
+        put(Material.STONE_AXE, false);
+        put(Material.STONE_SPEAR, false);
         put(Material.ENCHANTED_BOOK, false);
-        put(Material.FISHING_ROD, false);
         put(Material.EMERALD, false);
+        put(Material.COPPER_NAUTILUS_ARMOR, false);
+        put(Material.IRON_NAUTILUS_ARMOR, false);
         put(Material.LEATHER_CHESTPLATE, false);
         put(Material.GOLDEN_APPLE, false);
         put(Material.GOLDEN_HELMET, false);
+        put(Material.GOLDEN_NAUTILUS_ARMOR, false);
+        put(Material.DIAMOND_NAUTILUS_ARMOR, false);
     }};
 
+    private final HashMap<Material, Boolean> enchantedLoot = new HashMap<>() {{
+        put(Material.FISHING_ROD, false);
+    }};
+
+
     public static int getItemCount() {
-        return 10;
+        return 17;
     }
 
     @Override
@@ -32,5 +43,9 @@ public class OceanRuin extends Structure {
     @Override
     public @NotNull HashMap<Material, Boolean> getLoot() {
         return loot;
+    }
+
+    public HashMap<Material, Boolean> getEnchantedLoot() {
+        return enchantedLoot;
     }
 }
